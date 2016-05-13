61b9388024974b7d7f98fd270124b7ebf1276a1f
matthijahn <matthijahn@users.noreply.github.com>
2016-05-13 14:59:34 +0200

    Update redmine_install_ubuntu1404.sh
    Generate init script

--- redmine_install_ubuntu1404.sh
+++ redmine_install_ubuntu1404.sh
@@ -187,4 +187,84 @@ su - $s_user_alias -c "cd redmine && gem install bundler"
 su - $s_user_alias -c "cd redmine && rbenv rehash"
 su - $s_user_alias -c "cd redmine && bundle install --without development test"
 
+echo "generate Token"
+
+su - $s_user_alias -c "cd redmine && rake generate_secret_token"
+su - $s_user_alias -c "cd redmine && RAILS_ENV=production rake db:migrate"
+su - $s_user_alias -c "cd redmine && RAILS_ENV=production rake redmine:load_default_data"
+
+echo "Configure Initscript"
+(
+cat <<'EOF'
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          redmine
+# Required-Start:    $local_fs $remote_fs $network $syslog
+# Required-Stop:     $local_fs $remote_fs $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts redmine with puma
+# Description:       Starts redmine from /home/redmine/redmine.
+### END INIT INFO
+
+# Do NOT "set -e"
+
+EOF
+) > /etc/init.d/redmine
+
+echo "APP_USER=$s_user_alias" >> /etc/init.d/redmine
+
+(
+cat <<'EOF'
+APP_NAME=redmine
+APP_ROOT="/home/$APP_USER/$APP_NAME"
+RAILS_ENV=production
+
+RBENV_ROOT="/home/$APP_USER/.rbenv"
+PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
+SET_PATH="cd $APP_ROOT; rbenv rehash"
+DAEMON="bundle exec puma"
+DAEMON_ARGS="-C $APP_ROOT/config/puma.rb -e $RAILS_ENV"
+CMD="$SET_PATH; $DAEMON $DAEMON_ARGS"
+NAME=redmine
+DESC="Redmine Service"
+PIDFILE="$APP_ROOT/tmp/pids/puma.pid"
+SCRIPTNAME="/etc/init.d/$NAME"
+
+cd $APP_ROOT || exit 1
+
+sig () {
+        test -s "$PIDFILE" && kill -$1 `cat $PIDFILE`
+}
+
+case $1 in
+  start)
+        sig 0 && echo >&2 "Already running" && exit 0
+        su - $APP_USER -c "$CMD"
+        ;;
+  stop)
+        sig QUIT && exit 0
+        echo >&2 "Not running"
+        ;;
+  restart|reload)
+        sig USR2 && echo "Restarting" && exit 0
+        echo >&2 "Couldn't restart"
+        ;;
+  status)
+        sig 0 && echo >&2 "Running " && exit 0
+        echo >&2 "Not running" && exit 1
+        ;;
+  *)
+        echo "Usage: $SCRIPTNAME {start|stop|restart|status}" >&2
+        exit 1
+        ;;
+esac
+
+:
+EOF
+) >> /etc/init.d/redmine
+
+chmod +x /etc/init.d/redmine
+update-rc.d redmine defaults
+
 exit 0
